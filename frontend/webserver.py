421da3d5d040af3f4d8cc0cb934de6568cdd27fc
lukasWastaken <l.leutgeb@students.htl-leonding.ac.at>
2025-03-12 19:15:16 +0100

    Implemented frontend

--- frontend/webserver.py
+++ frontend/webserver.py
@@ -1,26 +1,24 @@
-from flask import Flask
-from flask import send_file
-from flask import request
+from flask import Flask, render_template, request, redirect, url_for
 import ai.invoke as invoke
 import ai.agent as agent
-
 
 app = Flask(__name__)
 
 @app.route("/")
 def mainWebsite():
-    try:
-        return send_file('index.html')
-    except Exception as e:
-       return str(e)
+    return render_template('index.html')
 
-@app.route("/test")
+@app.route("/prompt", methods=["GET", "POST"])
 def sendPrompt():
-    prompt = request.args.get('prompt')
-    result = agent.solve_code(prompt)
-    summary = agent.summarize_code(result)    
-    output = [result, summary]
-    return output
+
+    prompt = request.form.get('prompt') if request.method == "POST" else request.args.get('prompt')
+    if prompt:
+        result = agent.solve_code(prompt)
+        summary = agent.summarize_code(result)
+
+        return render_template('result.html', result=result, summary=summary)
+    else:
+        return redirect(url_for('mainWebsite'))
 
 def run():
     app.run(debug=True)
