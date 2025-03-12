5739715d44790351072652c3510c536c1c07c1c3
lukasWastaken <l.leutgeb@students.htl-leonding.ac.at>
2025-03-12 18:56:24 +0100

    Added first webserver and frontend

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import ai.invoke as invoke
 import ai.agent as agent
-
-result = agent.solve_code("Write a bubble sort algorithm in C#.")
+import frontend.webserver as ws
+""" result = agent.solve_code("Write a bubble sort algorithm in C#.")
 summary = agent.summarize_code(result)
 
 print("------------------------------")
 print(result)
-print(summary)
+print(summary) """
+
+ws.run()
